b2562c0d9dfbb2367a5023d02dcccf175d20e42b
Alina <tereshenko44@icloud.com>
2025-01-25 17:14:39 +0200

    Added fixed header functionality on scroll, fixed user button and corrected arrow positioning for navigation menu in the mobile version

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
 $('#demo').hideShowPassword({
     innerToggle: true
 });
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    if (window.scrollY > header.offsetTop) {
+        header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
+});
